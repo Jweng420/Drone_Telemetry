5b99bb16440b76b16b5945d52c9a49fc322918fe
Jweng420 <jjw0034@uah.edu>
2024-02-06 10:07:41 -0600

    Installed tilesfx via maven

--- Tilesfx/src/main/java/org/example/Main.java
+++ Tilesfx/src/main/java/org/example/Main.java
@@ -1,10 +1,75 @@
 package org.example;
-//package eu.hansolo;
+
+import eu.hansolo.tilesfx.Tile.ChartType;
+import eu.hansolo.tilesfx.Tile.ImageMask;
+import eu.hansolo.tilesfx.Tile.MapProvider;
+import eu.hansolo.tilesfx.Tile.SkinType;
+import eu.hansolo.tilesfx.Tile.TileColor;
+import eu.hansolo.tilesfx.addons.HappinessIndicator;
+import eu.hansolo.tilesfx.addons.HappinessIndicator.Happiness;
+import eu.hansolo.tilesfx.addons.Indicator;
+import eu.hansolo.tilesfx.chart.ChartData;
+import eu.hansolo.tilesfx.chart.RadarChartMode;
+import eu.hansolo.tilesfx.chart.SunburstChart.TextOrientation;
+import eu.hansolo.tilesfx.chart.TilesFXSeries;
+import eu.hansolo.tilesfx.colors.Bright;
+import eu.hansolo.tilesfx.colors.ColorSkin;
+import eu.hansolo.tilesfx.colors.Dark;
+import eu.hansolo.tilesfx.skins.BarChartItem;
+import eu.hansolo.tilesfx.skins.LeaderBoardItem;
+import eu.hansolo.tilesfx.tools.FlowGridPane;
+import eu.hansolo.tilesfx.tools.Helper;
+import eu.hansolo.tilesfx.tools.MatrixIcon;
+import eu.hansolo.tilesfx.tools.Rank;
+import eu.hansolo.tilesfx.tools.Ranking;
+import eu.hansolo.tilesfx.tools.TreeNode;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
+
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
@@ -14,5 +79,10 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
